d11650321101af5c08e5fba675334356d4a025b7
Jo du Plessis <jo@joduplessis.com>
2020-08-06 07:59:19 +0200

    Font updates

--- pages/termsofuse.js
+++ pages/termsofuse.js
@@ -12,7 +12,8 @@ export default function TermsOfUse(props) {
         <title>Weekday - Give your team superpowers</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="/static/images/favicon.png" rel="shortcut icon" />
-        <link rel="stylesheet" href="https://use.typekit.net/njt5tyh.css" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
 
       <style global jsx>{`
@@ -22,7 +23,7 @@ export default function TermsOfUse(props) {
         }
 
         h1, h2, h3, h4, h5, h6, button {
-          font-family: aktiv-grotesk-extended, sans-serif;
+          font-family: 'Montserrat', sans-serif;
           -webkit-font-smoothing: antialiased;
           text-rendering: optimizeLegibility;
         }
